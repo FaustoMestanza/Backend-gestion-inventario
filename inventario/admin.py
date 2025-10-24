41c5006d6ed469d6c0a966bbfe3225590616f384
FaustoMestanza <134259861+FaustoMestanza@users.noreply.github.com>
2025-10-24 13:42:44 -0500

    Mostar id en panel Django

--- inventario/admin.py
+++ inventario/admin.py
@@ -4,6 +4,7 @@ from .models import Equipo
 
 @admin.register(Equipo)
 class EquipoAdmin(admin.ModelAdmin):
-    list_display = ("codigo", "nombre", "categoria", "estado", "ubicacion", "fecha_registro")
+    list_display = ("id","codigo", "nombre", "categoria", "estado", "ubicacion", "fecha_registro")
     search_fields = ("codigo", "nombre", "categoria", "ubicacion")
     list_filter = ("estado", "categoria")
+    readonly_fields = ('id',)
