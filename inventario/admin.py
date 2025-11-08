63a842099961dba473e0668765048790cd1ba848
FaustoMestanza <134259861+FaustoMestanza@users.noreply.github.com>
2025-11-08 01:21:30 -0500

    Agregado campo registradoPor_id al modelo Equipo

--- inventario/admin.py
+++ inventario/admin.py
@@ -4,7 +4,13 @@ from .models import Equipo
 
 @admin.register(Equipo)
 class EquipoAdmin(admin.ModelAdmin):
+
     list_display = ("id","codigo", "nombre", "categoria", "estado", "ubicacion", "fecha_registro")
     search_fields = ("codigo", "nombre", "categoria", "ubicacion")
     list_filter = ("estado", "categoria")
     readonly_fields = ('id',)
+
+    list_display = ("codigo", "nombre", "categoria", "estado", "ubicacion","registradoPor_id", "fecha_registro")
+    search_fields = ("codigo", "nombre", "categoria", "ubicacion", "registradoPor_id")
+    list_filter = ("estado", "categoria", "registradoPor_id")
+
