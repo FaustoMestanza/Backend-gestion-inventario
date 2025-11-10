2982321261461853a30a3f10ebfff60c6ca8f3fd
FaustoMestanza <134259861+FaustoMestanza@users.noreply.github.com>
2025-11-09 23:37:08 -0500

    Fix: ajustes en model invemtario

--- inventario/admin.py
+++ inventario/admin.py
@@ -10,7 +10,7 @@ class EquipoAdmin(admin.ModelAdmin):
     list_filter = ("estado", "categoria")
     readonly_fields = ('id',)
 
-    list_display = ("codigo", "nombre", "categoria", "estado", "ubicacion","registradoPor_id", "fecha_registro")
-    search_fields = ("codigo", "nombre", "categoria", "ubicacion", "registradoPor_id")
-    list_filter = ("estado", "categoria", "registradoPor_id")
+    list_display = ("codigo", "nombre", "categoria", "estado", "ubicacion", "fecha_registro")
+    search_fields = ("codigo", "nombre", "categoria", "ubicacion" )
+    list_filter = ("estado", "categoria")
 
